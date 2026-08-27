3aa37502117984fe9535e6f38da25318acc5f780
Kauã Marques Almeida <08262975103@seguranca.local>
2026-08-27 16:55:55 -0400

    Feat: add ex12

---
+++ EX12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*12. Ler o saldo da conta, o limite diário de saque e o valor
+solicitado. Se o saldo for menor que o valor, informar saldo
+insuficiente e não realizar as verificações seguintes. Havendo
+saldo, verificar o limite: se o valor ultrapassar o limite diário,
+informar isso; caso contrário, efetuar o saque e imprimir o novo
+saldo.*/
+
+
+public class EX12{
+ public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Digite o saldo da sua conta: ");
+	int saldo = sc.nextInt();
+	System.out.println("Digite o saldo da sua conta: ");
+	int saldo = sc.nextInt();
+	System.out.println("Digite o saldo da sua conta: ");
+	int saldo = sc.nextInt();
+	}
+
+}
