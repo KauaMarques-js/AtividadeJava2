4c2e6b429fd399fbf24c0041f1c8cde9dbd40bfb
Kauã Marques Almeida <08262975103@seguranca.local>
2026-09-01 13:31:19 -0400

    Feat: integrador melhorado

--- INTEGRADOR.java
+++ INTEGRADOR.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
-/*Terminal de estacionamento. Ler o tipo de veículo, onde 1
-é carro e 2 é moto, e definir a tarifa por hora com switch: carro
-custa 8,00 e moto custa 5,00. Tipo diferente imprime opção
-inválida e o programa não realiza as etapas seguintes. Ler a
-quantidade de horas e calcular o valor. Ler se o cliente tem
-cupom, respondendo sim ou nao: havendo cupom, verificar se
-o valor é de 50,00 ou mais; nesse caso aplicar 20 por cento de
-desconto, caso contrário informar que o cupom vale apenas
-acima de 50,00. Em ifs independentes, imprimir aviso de
-permanência longa quando passar de 12 horas e aviso de
-cobrança de diária quando passar de 24 horas. Classificar a
-permanência com else if: até 2 horas curta, até 6 horas média,
-acima disso longa. Imprimir o valor final com duas casas*/
 public class integrador{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Tipo de veiculo: 1 - carro ; 2 - moto ;");
 	int tipoVei = sc.nextInt();
-   System.out.println("Ficou quantas horas? ");
+  System.out.println("Ficou quantas horas? ");
 	int horasFicadas = sc.nextInt();
+   boolean continuar = true;
+  if(horasFicadas >= 0 ){
+    
+  }else{
+    System.out.println("Horas invalidas, tente novamente");
+    continuar = false;
+  }
 	double tarifa = 0;
-	boolean continuar = true;
+	
 	switch(tipoVei){
 	case 1:
 	tarifa = 8.00 * horasFicadas;
@@ -29,7 +23,7 @@ public class integrador{
 	tarifa = 5.00 * horasFicadas;
 	break;
 	default:
-	System.out.println("Valor de veiculo invalido, tente novamente.");
+	System.out.println("Veiculo invalido, tente novamente.");
 	continuar = false;
 	}
 	
